3e80cfd60f3d238218011d3e18f11088d787ef7e
mavergarah <mavergarah@gmail.com>
2025-02-06 18:14:15 -0500

    OrderProduct and Order ingestion modified with foreign keys

--- mapana/ingestion/management/commands/ingest_data.py
+++ mapana/ingestion/management/commands/ingest_data.py
@@ -9,8 +9,8 @@ class Command(BaseCommand):
         parser.add_argument('departments_csv', type=str, help='Path to the departments CSV file')
         parser.add_argument('aisle_csv', type=str, help='Path to the products CSV file')
         parser.add_argument('products_csv', type=str, help='Path to the departments CSV file')
-#        parser.add_argument('order_product_csv', type=str, help='Path to the products CSV file')
-#        parser.add_argument('order_csv', type=str, help='Path to the products CSV file')
+        parser.add_argument('order_product_csv', type=str, help='Path to the products CSV file')
+        parser.add_argument('order_csv', type=str, help='Path to the products CSV file')
 
     def handle(self, *args, **options):
         csv_file = options['departments_csv']
@@ -76,5 +76,75 @@ class Command(BaseCommand):
                     )
                 except Exception as e:
                     self.stdout.write(self.style.ERROR(f"Error importing row {e}"))
+
+            self.stdout.write(self.style.SUCCESS(f"Data import completed"))
+
+    def handle(self, *args, **options):
+        csv_file = options['order_product_csv']
+        max_rows = 500000
+        row_count = 0
+
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                if row_count >= max_rows:
+                    break
+
+                try:
+                    order_id_instance = Order.objects.get(pk=row['order_id'])
+                    product_id_instance = Product.objects.get(pk=row['product_id'])
+                    model_instance = OrderProduct(
+                        order_id = order_id_instance,
+                        product_id = product_id_instance,
+                        aisle_id = row['aisle_id'],
+                        department_id = row['department_id']
+                    )
+                    model_instance.save()
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f"Successfully imported products"
+                        )
+                    )
+                except Exception as e:
+                    self.stdout.write(self.style.ERROR(f"Error importing row {e}"))
+
+                row_count += 1
+
+            self.stdout.write(self.style.SUCCESS(f"Data import completed"))
+
+    def handle(self, *args, **options):
+        csv_file = options['order_csv']
+        max_rows = 100000
+        row_count = 0
+
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                if row_count >= max_rows:
+                    break
+
+                try:
+                    user_id_instance = Department.objects.get(pk=row['user_id'])
+                    model_instance = Order(
+                        order_id = row['order_id'],
+                        user_id = user_id_instance,
+                        eval_set = row['eval_set'],
+                        order_number = row['order_number'],
+                        order_dow = row['order_dow'],
+                        order_hour_of_day = row['order_hour_of_day'],
+                        days_since_prior_order = row['days_since_prior_order']
+                    )
+                    model_instance.save()
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f"Successfully imported orders"
+                        )
+                    )
+                except Exception as e:
+                    self.stdout.write(self.style.ERROR(f"Error importing row {e}"))
+
+                row_count += 1
 
             self.stdout.write(self.style.SUCCESS(f"Data import completed"))
