0f942777d2a1af9cfd50dc2d2e293a4c90168012
mavergarah <mavergarah@gmail.com>
2025-02-06 16:12:35 -0500

    Products and aisles ingestion modified

--- mapana/ingestion/management/commands/ingest_data.py
+++ mapana/ingestion/management/commands/ingest_data.py
@@ -7,10 +7,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('departments_csv', type=str, help='Path to the departments CSV file')
+        parser.add_argument('aisle_csv', type=str, help='Path to the products CSV file')
         parser.add_argument('products_csv', type=str, help='Path to the departments CSV file')
-        parser.add_argument('aisle_csv', type=str, help='Path to the departments CSV file')
-        parser.add_argument('order_product_csv', type=str, help='Path to the departments CSV file')
-        parser.add_argument('order_csv', type=str, help='Path to the departments CSV file')
+#        parser.add_argument('order_product_csv', type=str, help='Path to the products CSV file')
+#        parser.add_argument('order_csv', type=str, help='Path to the products CSV file')
 
     def handle(self, *args, **options):
         csv_file = options['departments_csv']
@@ -34,29 +34,6 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS(f"Data import completed"))
 
     def handle(self, *args, **options):
-        csv_file = options['products_csv']
-        with open(csv_file, 'r') as file:
-            reader = csv.DictReader(file)
-
-            for row in reader:
-                try:
-                    model_instance = Product(
-                        product_name = row['product_name'],
-                        aisle_id = row['aisle_id'],
-                        department_id = row['department_id'],
-                    )
-                    model_instance.save()
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f"Successfully imported products"
-                        )
-                    )
-                except Exception as e:
-                    self.stdout.write(self.style.ERROR(f"Error importing row {e}"))
-
-            self.stdout.write(self.style.SUCCESS(f"Data import completed"))
-
-    def handle(self, *args, **options):
         csv_file = options['aisle_csv']
         with open(csv_file, 'r') as file:
             reader = csv.DictReader(file)
@@ -64,7 +41,7 @@ class Command(BaseCommand):
             for row in reader:
                 try:
                     model_instance = Aisle(
-                        aisle = row['aisle'],
+                        aisle = row['aisle']
                     )
                     model_instance.save()
                     self.stdout.write(
@@ -78,22 +55,18 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS(f"Data import completed"))
 
     def handle(self, *args, **options):
-        csv_file = options['order_products_csv']
-        max_rows = 500000
-        row_count = 0
-
+        csv_file = options['products_csv']
         with open(csv_file, 'r') as file:
             reader = csv.DictReader(file)
 
             for row in reader:
-                if row_count >= max_rows:
-                    break
-
                 try:
-                    model_instance = OrderProduct(
+                    aisle_instance = Aisle.objects.get(pk=row['aisle_id'])
+                    department_instance = Department.objects.get(pk=row['department_id'])
+                    model_instance = Product(
                         product_name = row['product_name'],
-                        aisle_id = row['aisle_id'],
-                        department_id = row['department_id']
+                        aisle_id = aisle_instance,
+                        department_id = department_instance
                     )
                     model_instance.save()
                     self.stdout.write(
@@ -103,42 +76,5 @@ class Command(BaseCommand):
                     )
                 except Exception as e:
                     self.stdout.write(self.style.ERROR(f"Error importing row {e}"))
-
-                row_count += 1
-
-            self.stdout.write(self.style.SUCCESS(f"Data import completed"))
-
-    def handle(self, *args, **options):
-        csv_file = options['order_csv']
-        max_rows = 100000
-        row_count = 0
-
-        with open(csv_file, 'r') as file:
-            reader = csv.DictReader(file)
-
-            for row in reader:
-                if row_count >= max_rows:
-                    break
-
-                try:
-                    model_instance = Order(
-                        order_id = row['order_id'],
-                        user_id = row['user_id'],
-                        eval_set = row['eval_set'],
-                        order_number = row['order_number'],
-                        order_dow = row['order_dow'],
-                        order_hour_of_day = row['order_hour_of_day'],
-                        days_since_prior_order = row['days_since_prior_order']
-                    )
-                    model_instance.save()
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f"Successfully imported orders"
-                        )
-                    )
-                except Exception as e:
-                    self.stdout.write(self.style.ERROR(f"Error importing row {e}"))
-
-                row_count += 1
 
             self.stdout.write(self.style.SUCCESS(f"Data import completed"))
