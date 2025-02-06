6213ce0f8ad267dc8086613982a253f3b5d9fc84
mavergarah <mavergarah@gmail.com>
2025-02-06 08:51:56 -0500

    Rest of models added to projecto with foreign keys

--- mapana/ingestion/models.py
+++ mapana/ingestion/models.py
@@ -14,3 +14,81 @@ class Department(models.Model):
 
     def __str__(self):
         return self.department
+
+class Aisle(models.Model):
+    aisle_id = models.AutoField(
+        primary_key=True
+    )
+    aisle = models.CharField(
+        max_length=50,
+        verbose_name='aisle',
+        blank=False,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.aisle
+
+class Product(models.Model):
+    product_id = models.AutoField(
+        primary_key=True
+    )
+    product_name = models.CharField(
+        max_length=50,
+        verbose_name='product_name',
+        blank=False,
+        unique=True
+    )
+
+    aisle_id = models.ForeignKey(
+        Aisle,
+        on_delete=models.CASCADE
+    )
+
+    department_id = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.product_name
+
+class User(models.Model):
+    user_id = models.AutoField(
+        primary_key=True
+    )
+    def __str__(self):
+        return self.id
+
+class Order(models.Model):
+    order_id = models.IntegerField()
+    user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    eval_set = models.CharField(
+        max_length=50,
+        blank=False,
+    )
+    order_number = models.IntegerField()
+    order_dow = models.IntegerField()
+    order_hour_of_day = models.IntegerField()
+    days_since_prior_order = models.IntegerField()
+
+    def __str__(self):
+        return self.order_id
+
+class OrderProduct(models.Model):
+    order_id = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE
+    )
+    product_id = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+    add_to_cart_order = models.IntegerField()
+    reordered = models.IntegerField()
+
+    def __str__(self):
+        return self.add_to_cart_order
